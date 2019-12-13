cf0756150a647d6149ac2160978205f661cefc76
Christopher Wagstaffe <cjwagstaffe@gmail.com>
2019-12-13 06:10:57 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,11 +26,8 @@ def my_hash_creator(key, value)
   p ro
 end
 
-my_hash_creator("Did I work?", "yes")
-
-
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
+  
 end
 
 def update_counting_hash(hash, key)
